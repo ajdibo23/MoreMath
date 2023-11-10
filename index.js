c4e63ff9698b7cd55a182295051dd289e5a75546
AJ DiBasilio <149754393+ajdibo23@users.noreply.github.com>
2023-11-10 00:04:14 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,117 @@
 const express = require('express')
 const fs = require('fs')
-var data = require('./accounts.json')
 const app = express()
-var path = require('path');
-var passport = require('passport')
-app.use(passport.initialize());
-const client = require('https');
+const User = require("./models/user");
+const ejs = require('ejs')
+const { v4: uuidv4 } = require("uuid");
+const cors = require('cors')
+const cookieParser = require("cookie-parser");
+const mongoose = require('mongoose')
 app.use(express.static(__dirname + '/public'));
-require('dotenv').config()
+app.use(cors())
+app.set('view engine', 'ejs')
+app.use(cookieParser());
 
-var users = {
-    "dir": './routes/users/', // requires the / at the end
-    "aftername": '/users'
-  }
-  fs.readdir(users.dir, (err, files) => {
-    files.forEach(file => {
-      app.use(users.aftername, require(users.dir + file));
-    });
+mongoose.connect('mongodb://localhost:27017/moremath', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+
+fs.readdir('./routes/users/', (err, files) => {
+  files.forEach(file => {
+    app.use('/users', require('./routes/users/' + file));
   });
+});
 
-  var math = {
-    "dir": './routes/math/', // requires the / at the end
-    "aftername": '/math'
-  }
-  fs.readdir(math.dir, (err, files) => {
-    files.forEach(file => {
-      app.use(math.aftername, require(math.dir + file));
-    });
+fs.readdir('./routes/math/', (err, files) => {
+  files.forEach(file => {
+    app.use('/math', require('./routes/math/' + file));
   });
+});
 
-  app.use(express.urlencoded({
-    extended: true
-  }))
-  
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.get('/', (req, res) => {
-    res.sendFile('/index.html', { root: __dirname });
+  if (req.cookies.moremath) {
+    let users = []
+    findUsers()
+    async function findUsers() {
+      let cookies = await JSON.parse(req.cookies.moremath);
+      for (let q = 0; q < cookies.length; q++) {
+        await User.findOne({ token: cookies[q].token }).then(async user => {
+          if (user) {
+            await users.push(user)
+          }
+        })
+        if (q == cookies.length - 1) {
+          res.render('index', { users: users })
+        }
+      }
+    }
+  }
+});
+
+app.get('/jquery.js', (req, res) => {
+  res.sendFile('/jquery.js', { root: __dirname });
+});
+
+app.get('/bootstrap.js', (req, res) => {
+  res.sendFile('/bootstrap.js', { root: __dirname });
+});
+
+app.get('/bootstrap.css', (req, res) => {
+  res.sendFile('/bootstrap.css', { root: __dirname });
 });
 
 app.get('/practice', (req, res) => {
-    res.sendFile('/practice.html', { root: __dirname });
+  if (req.cookies.current) {
+    User.findOne({ token: req.cookies.current }).then(user => {
+      if (user) {
+        res.render('practice', { user: user })
+      } else {
+        res.redirect('/')
+      }
+    })
+  } else {
+    res.redirect('/')
+  }
+});
+
+app.get('/complete', (req, res) => {
+  if (req.cookies.current) {
+    User.findOne({ token: req.cookies.current }).then(user => {
+      if (user) {
+        res.render('complete', { user: user })
+      } else {
+        res.redirect('/')
+      }
+    })
+  } else {
+    res.redirect('/')
+  }
 });
 
 app.get('/signup', (req, res) => {
-    res.sendFile('/signup.html', { root: __dirname });
+  res.sendFile('/signup.html', { root: __dirname });
 });
 
 app.get('/login', (req, res) => {
   res.sendFile('/login.html', { root: __dirname });
 });
-fs.writeFileSync('accounts.json', JSON.stringify(data, null, "\t"));
+
+app.post('/start', (req, res) => {
+  if (req.cookies.moremath) {
+    User.findOne({ token: req.body.token }).then(user => {
+      if (user) {
+        user.session = uuidv4();
+        user.save()
+        res.cookie('current', user.session, { maxAge: 900000, httpOnly: true });
+        res.send({
+          success: true
+        })
+      }
+    })
+  }
+});
+
 app.listen(80)
